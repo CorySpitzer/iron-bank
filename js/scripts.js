d5cccf7a887c9d365aae8566644b19f23d0d636a
Cory Spitzer & Frank Johnson <student@epicodus.com>
2015-06-02 14:17:03 -0700

    Add css and get input from forms

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,24 @@ BankAccount.prototype.deposit = function(amount) {
 BankAccount.prototype.withdraw = function(amount) {
   return this.balance -= amount;
 };
+
+$(document).ready(function() {
+  $('form#new-account').submit(function(event) {
+    event.preventDefault();
+    var holderName = $('input#account-holder-name').val();
+    var initialDeposit = parseInt($('input#initial-deposit').val());
+    // alert(initialDeposit + ' ' + HolderName);
+    account = new BankAccount(holderName, initialDeposit);
+    // alert(account.holderName + initialDeposit)
+  });
+
+  $('form#alter-account').submit(function(event) {
+    event.preventDefault();
+    var depositAmount = parseInt($('input#account-deposit').val());
+    var withdrawAmount = parseInt($('input#account-withdraw').val());
+    alert(depositAmount + ' ' + withdrawAmount);
+    var newBalance = account.deposit(depositAmount - withdrawalAmount);
+    newBalance = 5;
+    alert(newBalance);
+  });
+});
